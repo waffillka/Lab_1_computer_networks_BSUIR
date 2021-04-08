8263030d338fa4e331997931efe324f3b2c4890d
waffillka <waffilka@gmail.com>
2021-04-08 22:22:01 +0300

    Add project files.

---
+++ lab_1.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace lab_1._1
+{
+    class Program
+    {
+        static int port = 8005;
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.ReceiveTimeout = 30000;
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(10);
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+
+                    Socket handler = listenSocket.Accept();
+
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (handler.Available > 0);
+
+                    Console.WriteLine("Client: " + builder.ToString());
+
+                    data = Encoding.Unicode.GetBytes(Regex.Replace(builder.ToString(), "[ ]+", " "));
+                    handler.Send(data);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
